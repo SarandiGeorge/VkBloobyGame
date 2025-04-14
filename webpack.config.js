36951243e9f01d878d17d491b7638f8f4d202a75
SarandiGeorge <Sarandigeorge@gmail.com>
2025-04-14 17:21:36 +0300

    Updated webpack.config.js with HtmlWebpackPlugin and fixed devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,51 @@
 const path = require('path');
+// Плагин для копирования index.html и других статических файлов
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // Точка входа
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'), // Папка для сборки
+    filename: 'bundle.js', // Имя выходного файла
+    publicPath: '/', // Публичный путь для ресурсов
+    clean: true, // Очищать dist перед сборкой
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/, // Обработка JS/JSX файлов
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|wav)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]',
-            context: 'src',
-          },
-        },
-      },
-    ],
+        test: /\.(png|jpg|jpeg|gif|ico|wav|mp3)$/, // Обработка активов
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]' // Копировать активы в dist/assets/
+        }
+      }
+    ]
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Используем public/ для статических файлов
+    },
+    port: 9000,
+    https: true,
+    historyApiFallback: true, // Перенаправлять все запросы на index.html
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico',
-    }),
+      template: './public/index.html', // Использовать public/index.html как шаблон
+      filename: 'index.html', // Имя файла в dist/
+    })
   ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    https: true,
+  resolve: {
+    extensions: ['.js', '.jsx'], // Разрешённые расширения
   },
 };
